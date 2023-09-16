81894907ebb61d38e7956627b92f861ec5ea820d
Squirtle22 <djkeel@live.co.uk>
2023-09-16 11:24:45 +0100

    updated verison
    working weather app gets current weathers using API, search bar working, current location, time and date

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,52 +1,103 @@
-const apiKey = '';
-// Get the current date and time
-function updateDatetime() {
-const currentDatetime = new Date();
-const formattedDatetime = currentDatetime.toLocaleString();
-document.getElementById("currentDatetime").textContent = formattedDatetime;
+// API key for OpenWeatherMap
+const apiKey = ''; //api key needed get from https://openweathermap.org/api
+
+// Variables to store city name, latitude, and longitude
+let cityName = ''; // Initialize with the default city name
+let latitude = '';
+let longitude = '';
+
+// DOM element for unit link (Celsius/Fahrenheit)
+const unitLink = document.getElementById("unitLink");
+
+// Function to get the current date and time
+function getCurrentDatetime() {
+  const currentDatetime = new Date();
+  return currentDatetime.toLocaleString();
 }
 
-// Initially, call the updateDatetime function to display the current date and time
-updateDatetime();
+// Function to update the date and time displayed on the webpage
+function updateDatetime() {
+  const formattedDatetime = getCurrentDatetime();
+  document.getElementById("currentDatetime").textContent = formattedDatetime;
+}
 
-// Set up an interval to update the time every second (1000 milliseconds)
-setInterval(updateDatetime, 1000);
+// Function to make the API call to fetch weather data
+function getAPICall(search) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?${search}&appid=${apiKey}&units=metric`;
 
-// Function to fetch weather data using coordinates and display it
-function fetchWeatherDataAndDisplay(latitude, longitude ) {
-  // Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
-
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-  fetch(apiUrl)
+  return fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.name)
-      console.log(data.main)
+      // Extract weather data from the API response
       const city = data.name;
-      const temperature = data.main.temp;      
-      const unitLink = document.getElementById("unitLink");
+      const temperatureCelsius = data.main.temp;
+      const humidity = data.main.humidity;
+      const pressure = data.main.pressure;
+      const temp_max = data.main.temp_max;
+      const temp_min = data.main.temp_min;
+      const wind = Math.round(data.wind.speed);
+      const windDirection = data.wind.deg;
+      const condition = data.weather[0].description;
+      const windDirectionCardinal = degreesToCardinal(windDirection);
       
-      // Display the city and temperature on the page
-      document.getElementById("cityName").textContent = city;
-      document.getElementById("temperatureValue").textContent = Math.round(temperature);
-      unitLink.textContent = "°C";
+      // Log data and condition to the console
+      console.log(data);
+      console.log(condition);
+
+      // Update elements on the page with the fetched weather data
+      updateElementText("cityName", city);
+      updateElementText("temperatureValue", Math.round(temperatureCelsius));
+      updateElementText("humidity", `Humidity: ${humidity}%`);
+      updateElementText("wind", `Wind: ${wind} mph`);
+      updateElementText("windDirection", `Wind Direction: ${windDirectionCardinal}`);
+      updateElementText("condition", `Condition: ${condition}`);
     })
     .catch((error) => {
+      // Handle errors and display an alert for an invalid city name
       console.error("Error fetching weather data:", error);
+      alert("Invalid city name. Please enter a valid city name.");
     });
 }
 
-// Function to handle button click
+// Function to handle the search button click event
+function handleSearch() {
+  // Get the city name entered by the user from the input field
+  cityName = document.getElementById("cityInput").value;
+
+  // Check if the city name is empty
+  if (cityName.trim() === "") {
+    alert("Please enter a valid city name.");
+    return;
+  }
+
+  // Check if the city name contains only letters and spaces (validity check)
+  if (!/^[A-Za-z\s]+$/.test(cityName)) {
+    alert("Invalid city name. Please enter a valid city name.");
+    return;
+  }
+  
+  // Call the function to make the API call with the formatted city name
+  getAPICall(`q=${cityName}`);
+}
+
+// Function to handle Enter key press in the input field
+function handleEnterKeyPress(event) {
+  if (event.key === "Enter") {
+    handleSearch(); // Call the handleSearch function if Enter key is pressed
+  }
+}
+
+// Function to handle the current location button click event
 function handleCurrentLocationButtonClick() {
   if (navigator.geolocation) {
+    // Get the user's current geolocation
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        
-        // Call the function to fetch and display weather data
-        fetchWeatherDataAndDisplay(latitude, longitude);
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+
+        // Call the function to make the API call with latitude and longitude
+        getAPICall(`lat=${latitude}&lon=${longitude}`);
       },
       (error) => {
         console.error("Geolocation error:", error);
@@ -58,66 +109,21 @@ function handleCurrentLocationButtonClick() {
   }
 }
 
-// Add a click event listener to the current location button
-document
-  .getElementById("currentLocationButton")
-  .addEventListener("click", handleCurrentLocationButtonClick);
-
-
-// Function to fetch weather data and return the temperature
-function getTemperature(cityName, callback) {
-  // Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
-  const apiKey = '';
-
-  // Create the API URL with the city name and API key (using imperial units for Fahrenheit)
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
-
-  // Use Axios to send the request to the OpenWeatherMap API
-  axios
-    .get(apiUrl)
-    .then(function (response) {
-      // Extract the temperature in Fahrenheit from the API response
-      const temperatureFahrenheit = response.data.main.temp;
-      console.log(response.data.main);
-
-      // Pass the temperature back to the callback function
-      callback(temperatureFahrenheit);
-    })
-    .catch(function (error) {
-      // Handle any errors that occur during the request
-      console.error(error);
-    });
-}
-
-document.getElementById("currentLocationButton").addEventListener("click", () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-      // Do something with the latitude and longitude, like display it on the screen.
-    }, (error) => {
-      // Handle error cases
-      console.error(error);
-    });
-  } else {
-    alert("Geolocation is not supported by this browser.");
-  }
-});
-
-// Convert Fahrenheit to Celsius
+// Function to convert Fahrenheit to Celsius
 function fahrenheitToCelsius(fahrenheit) {
   return (fahrenheit - 32) * 5 / 9;
 }
 
-function formatCityName(cityName) {
-  return cityName.charAt(0).toUpperCase() + cityName.slice(1);
+// Helper function to update element text content and the unit link
+function updateElementText(elementId, text) {
+  document.getElementById(elementId).textContent = text;
+  unitLink.textContent = "°C";
 }
 
-// Toggle temperature conversion between Celsius and Fahrenheit
+// Function to toggle between Celsius and Fahrenheit units
 function toggleTemperatureUnit() {
   const temperatureElement = document.getElementById("temperatureValue");
   const currentTemperature = parseInt(temperatureElement.textContent);
-  const unitLink = document.getElementById("unitLink");
 
   if (unitLink.textContent === "°C") {
     // Switch to Fahrenheit
@@ -132,35 +138,27 @@ function toggleTemperatureUnit() {
   }
 }
 
-// Function to handle the search button click event
-function handleSearch() {
-  // Get the city name entered by the user from the HTML input element
-  const cityNameInput = document.getElementById("cityInput").value;
-
-  // 
-  const unitLink = document.getElementById("unitLink");
-  // Format the city name
-  const formattedCityName = formatCityName(cityNameInput);
-
-  // Call the getTemperature function with the formatted city name
-  getTemperature(formattedCityName, function (temperatureFahrenheit) {
-
-    // Convert the temperature to Celsius
-    const temperatureCelsius = fahrenheitToCelsius(temperatureFahrenheit);
-
-    // Display the formatted city name in the HTML
-    document.getElementById("cityName").textContent = formattedCityName;
-
-    // Update the existing temperature element in the HTML with the converted temperature
-    const temperatureElement = document.getElementById("temperatureValue");
-    temperatureElement.textContent = Math.round(temperatureCelsius);
-    unitLink.textContent = "°C";
-    // Here, you can perform any other actions with the temperature data
-  });
+// Function to convert degrees to cardinal directions
+function degreesToCardinal(degrees) {
+  const cardinals = ["North", "North East", "East", "South East", "South", "South West", "West", "North West"];
+  const index = Math.round((degrees % 360) / 45);
+  return cardinals[index];
 }
 
 // Add a click event listener to the search button
 document.getElementById("searchButton").addEventListener("click", handleSearch);
 
+// Add a key press event listener to the input field
+document.getElementById("cityInput").addEventListener("keypress", handleEnterKeyPress);
+
 // Add click event listeners to the unit links (Celsius and Fahrenheit)
 document.getElementById("unitLink").addEventListener("click", toggleTemperatureUnit);
+
+// Add a click event listener to the current location button
+document.getElementById("currentLocationButton").addEventListener("click", handleCurrentLocationButtonClick);
+
+// Initially, call the updateDatetime function to display the current date and time
+updateDatetime();
+
+// Set up an interval to update the time every second (1000 milliseconds)
+setInterval(updateDatetime, 1000);
